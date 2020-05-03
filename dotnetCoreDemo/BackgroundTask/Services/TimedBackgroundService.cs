80d9ad55a44dcc3fb5e60f56fb375fe041f2963d
moling3650 <365024424@qq.com>
2020-05-03 16:37:05 +0800

    feat(BackgroundTask): 添加后台任务的示例

---
+++ dotnetCoreDemo/BackgroundTask/Services/TimedBackgroundService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackgroundTask.Services
+{
+    public class TimedBackgroundService : BackgroundService
+    {
+        private Timer _timer;
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            Console.WriteLine($"Hello World! - {DateTime.Now}");
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _timer?.Dispose();
+        }
+    }
+}
